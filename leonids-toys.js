a77e622cceee99735003075ecac73ea86b0361dd
Hayatsa <hayatabudiab@gmail.com>
2021-11-11 18:28:30 -0600

    first commit

---
+++ leonids-toys.js
@@ -0,0 +1,25 @@
+const toys = [
+    {
+        id: 1,
+        name: "Snorlax Plush",
+        maker: "Pokemon Official",
+        age: "All Ages",
+        price: 40 
+    },
+    {
+        id: 2,
+        name: "Rubik's Cube",
+        maker: "Rubik's Brand Ltd",
+        age: 8,
+        price: 20
+    },
+    {
+        id: 3,
+        name: "Jump Rope",
+        maker: "Mattel",
+        age: 5,
+        price: 10
+    }
+]
+
+console.log(toys)
